1d39ca76caca1e3da0bda0156f2cbef0d692cd3e
peegggy <peggyyuanvan@gmail.com>
2021-05-25 10:23:20 -0700

    first commit

--- task1/init.sv
+++ task1/init.sv
@@ -4,4 +4,28 @@ module init(input logic clk, input logic rst_n,
 
 // your code here
 
+
+
+always_ff @(posedge clk) begin
+    if(~en && ~rst_n)begin
+         rdy = 1; //ready to write to RAM
+         wren = 1; //to write to RAM
+         addr = 8'b0; //starting from addr 0
+
+    end
+    else if(~en && addr <= 9'd255)begin 
+            rdy = 1; //ready to write to RAM
+            wren = 1; 
+            addr = addr + 1; //increment by one spot
+        end
+
+    else begin
+        rdy = 0; //else not ready to write to RAM
+        wren = 0;
+        addr = addr;
+    end
+end
+
+assign wrdata = addr;
+
 endmodule: init
--- task1/task1.sv
+++ task1/task1.sv
@@ -1,11 +1,40 @@
+`timescale 1ps / 1ps
+
 module task1(input logic CLOCK_50, input logic [3:0] KEY, input logic [9:0] SW,
              output logic [6:0] HEX0, output logic [6:0] HEX1, output logic [6:0] HEX2,
              output logic [6:0] HEX3, output logic [6:0] HEX4, output logic [6:0] HEX5,
              output logic [9:0] LEDR);
 
     // your code here
+    logic rst_n;
+    assign rst_n = KEY[3];// KEY has reverse logic, therefore when KEY[3] is 0 it is actually 1 for rst_n
+    logic en, rdy;
+    logic [7:0] addr;
+    logic [7:0] wrdata;
+    logic [7:0] read_value;
+    logic wren;
 
-    s_mem s( /* connect ports */ );
+    always_comb begin
+        if(rdy)
+        en = 1'b1;
+        else en = 0;
+    end
+    init i(.clk(CLOCK_50),
+            .rst_n(rst_n),
+            .en(en),
+            .rdy(rdy),
+            .addr(addr),
+            .wrdata(wrdata),
+            .wren(wren));
+
+   
+    s_mem s(.address(addr),
+            .clock(CLOCK_50),
+            .data(wrdata),
+            .wren(wren),
+            .q(read_value));
+  
+
 
     // your code here
 
